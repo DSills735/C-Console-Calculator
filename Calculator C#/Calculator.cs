9918f60ebb6246253a742f1f8bf7c079933122af
David Sills <David.sills1000@gmail.com>
2023-05-24 15:10:57 -0500

    Add project files.

---
+++ Calculator C#/Calculator.cs
@@ -0,0 +1,31 @@
+﻿class Calculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; 
+
+        switch (op)
+        {
+            case "a":
+                Console.WriteLine($"Your Result: {num1} + {num2} = " + (num1 + num2));
+                break;
+            case "s":
+                Console.WriteLine($"Your Result: {num1} - {num2} = " + (num1 - num2));
+                break;
+            case "m":
+                Console.WriteLine($"Your Result: {num1} * {num2} = " + (num1 * num2));
+                break;
+            case "d":
+                while (num2 == 0)
+                {
+                    Console.WriteLine("Enter a Non-Zero Divisor:  ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine($"Your Result: {num1} / {num2} = " + (num1 / num2));
+
+                break;
+
+        }
+
+    }
+}
---
+++ Calculator C#/Program.cs
@@ -0,0 +1,22 @@
+﻿double num1 = 0; double num2 = 0;
+
+Console.WriteLine("Welcome to David's Console Calculator!");
+Console.WriteLine("You will be asked to type in 2 numbers. Please type them in the order you want them calculated.");
+Console.WriteLine("-----------------------------------------------------------------------------------------------\n");
+
+Console.WriteLine("Type the first number, then press Enter!");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please type the second number, then press enter!");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please choose the calculation from the following list.");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.WriteLine("Type your choice and press enter.");
+
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
